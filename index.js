57fb892ce9f2c9cac1b3ce464418f0b605c430a5
zrdzn <zrdzn@users.noreply.github.com>
2018-12-15 14:38:29 +0100

    Stworzenie podstawowych funkcjonalności w bocie

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const {token} = require('./config.json');
+const bot = new Discord.Client();
+bot.on('ready', () => {
+	console.log('\nBot zostal wlaczony');
+});
+bot.on("guildMemberAdd", function(member) {
+    member.guild.channels.find("name", "random").send(member.toString() + " dołączył na nasz serwer!")
+});
+bot.on("guildMemberRemove", function(member) {
+	member.guild.channels.find("name", "random").send(member.toString() + " wyszedł z naszego serwera!")
+});
+bot.login(token);
