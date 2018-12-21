061fb5376b214396e80ec25445873176d83da224
Szymon <tirexgta@gmail.com>
2018-12-21 19:01:15 +0100

    update server RichEmbed

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const Discord = require('discord.js');
-const {token, prefix} = require('./config.json');
+const {
+    token,
+    prefix
+} = require('./config.json');
 const bot = new Discord.Client();
 const commands = require("./commands");
 
 bot.on('ready', () => {
-	console.log('\nBot zostal wlaczony');
-	bot.user.setActivity("lsmc.pl", {type: "WATCHING"});
+    console.log('\nBot zostal wlaczony');
+    bot.user.setActivity("lsmc.pl", {
+        type: "WATCHING"
+    });
 });
 bot.on("guildMemberAdd", function(member) {
     member.guild.channels.find("name", "random").send(member.toString() + " dołączył na nasz serwer!")
 });
 bot.on("guildMemberRemove", function(member) {
-	member.guild.channels.find("name", "random").send(member.toString() + " wyszedł z naszego serwera!")
+    member.guild.channels.find("name", "random").send((member.nickname + "#" + member.user.discriminator) + " wyszedł z naszego serwera!")
 });
 bot.on('message', message => {
-    var cmdchannels = ["apokryf"];
+    var cmdchannels = ["apokryf", "testowy"];
     if (!cmdchannels.includes(message.channel.name)) {
         return;
     }
-    if (message.content.indexOf(prefix) !== 0)
-    {
+    if (message.content.indexOf(prefix) !== 0) {
         return;
     }
     const args = message.content.slice(prefix.length).trim().split(/\s+/g);
-    if (commands.callCommand(args, message))
-    {
+    if (commands.callCommand(args, message)) {
         return;
     }
 });
